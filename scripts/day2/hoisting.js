8df3f87e01049691e5a14746e1119d2d8ef98db1
evondev <trananhtuan400@gmail.com>
2023-08-11 22:03:37 +0700

    update source code day2

---
+++ scripts/day2/hoisting.js
@@ -0,0 +1,15 @@
+// Temporal Dead Zone
+// TDZ start here
+console.log(studentName);
+// TDZ start here
+// TDZ start here
+// TDZ start here
+let studentName = "evondev";
+// TDZ end here
+console.log(keyboardName);
+var keyboardName = "Keychron K2";
+// # Compile
+var keyboardName;
+console.log(keyboardName);
+keyboardName = "Keychron K2";
+//
